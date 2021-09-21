bcf2ee26deb5dab3ce4ff09ded3738be1119a14e
AhmetAtila <ahmetmatila@gmail.com>
2021-09-21 09:26:05 -0600

    update

--- Scripts/LandscapePortrait.js
+++ Scripts/LandscapePortrait.js
@@ -1,1 +1,13 @@
 console.log("rhrhher");
+
+let stop = false;
+
+function javascript () {
+
+}
+
+function main () {
+  let firstNumber, secondNumber;
+  firstNumber = document.getElementByID("textField1").value;
+
+}
