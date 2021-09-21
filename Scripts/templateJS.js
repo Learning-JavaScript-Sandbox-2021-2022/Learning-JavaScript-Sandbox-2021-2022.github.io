26960e935d929f96c101da97ba655a3bcbab5c7f
AhmetAtila <ahmetmatila@gmail.com>
2021-09-21 09:59:00 -0600

    update

--- Scripts/templateJS.js
+++ Scripts/templateJS.js
@@ -1,1 +1,30 @@
 console.log("asoiubosf");
+
+
+let stop = false;
+
+function javascript () {
+
+}
+
+function main () {
+  let firstNumber, secondNumber;
+  firstNumber = document.getElementByID("textField1").value;
+  secondNumber = document.getElementByID("textfield2").value;
+
+
+  document.getElementByID("validityTest1").innerHTML = alert (testNAN (firstNumber) );
+  document.getElementByID("validityTest2").innerHTML = testNAN (firstNumber);
+  document.getElementByID("validityTest1").innerHTML = alert (testNAN (secondNumber) );
+  document.getElementByID("validityTest2").innerHTML = testNAN (secondNumber);
+}
+
+function testNAN (number) {
+  if ( isNAN (number) ) { //
+    stop = ture;
+    return "Type a Real Number"
+  } else {
+    return "Input Validated"
+  }
+
+}
