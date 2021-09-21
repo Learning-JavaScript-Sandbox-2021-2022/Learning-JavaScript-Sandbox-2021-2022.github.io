ca59241990b34e60609ce97b2dba9aaa6e47b0ae
AhmetAtila <ahmetmatila@gmail.com>
2021-09-21 09:39:53 -0600

    Update LandscapePortrait.js

--- Scripts/LandscapePortrait.js
+++ Scripts/LandscapePortrait.js
@@ -10,4 +10,15 @@ function main () {
   let firstNumber, secondNumber;
   firstNumber = document.getElementByID("textField1").value;
 
+  document.getElementByID("validityTest1").innerHTML = alert (testNAN (firstNumber) );
+}
+
+function testNAN (number) {
+  if ( isNAN (number) ) { //
+    stop = ture;
+    return "Type a Real Number"
+  } else {
+    return "Input Validated"
+  }
+
 }
