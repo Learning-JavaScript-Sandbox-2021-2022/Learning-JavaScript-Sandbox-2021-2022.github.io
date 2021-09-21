6304002d56eb4f088e6664e524fda8833576c179
AhmetAtila <ahmetmatila@gmail.com>
2021-09-21 09:51:19 -0600

    Update LandscapePortrait.js

--- Scripts/LandscapePortrait.js
+++ Scripts/LandscapePortrait.js
@@ -9,9 +9,13 @@ function javascript () {
 function main () {
   let firstNumber, secondNumber;
   firstNumber = document.getElementByID("textField1").value;
+  secondNumber = document.getElementByID("textfield2").value;
+
 
   document.getElementByID("validityTest1").innerHTML = alert (testNAN (firstNumber) );
-  document.getElementByID("validityTest2").innerHTML = alert (testNAN (secondNumber) );
+  document.getElementByID("validityTest2").innerHTML = testNAN (firstNumber);
+  document.getElementByID("validityTest1").innerHTML = alert (testNAN (secondNumber) );
+  document.getElementByID("validityTest2").innerHTML = testNAN (secondNumber);
 }
 
 function testNAN (number) {
