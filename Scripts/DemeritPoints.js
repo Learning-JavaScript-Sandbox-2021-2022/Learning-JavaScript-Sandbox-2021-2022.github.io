6269d5967d48402230080abba5306ab3b48005f0
AhmetAtila <ahmetmatila@gmail.com>
2021-10-05 09:41:12 -0600

    update

--- Scripts/DemeritPoints.js
+++ Scripts/DemeritPoints.js
@@ -1,30 +1,37 @@
-console.log("DemeritPoints baby!!!");
+document.getElementById("inputButton").addEventListener("click", speedFunction)
 
+function speedFunction() {
 
-let stop = false;
-
-function javascript () {
-
+if (speed >= 130){
+  document.getElementById("answer").innerHTML = "Your Lisence is Suspended";
+} else if (speed >= 125){
+  document.getElementById("answer").innerHTML = "11 Demerit Points";
+} else if (speed >= 120){
+  document.getElementById("answer").innerHTML = "10 Demerit Points"
+} else if (speed >= 115){
+  document.getElementById("answer").innerHTML = "9 Demerit Points"
+} else if (speed >= 110){
+  document.getElementById("answer").innerHTML = "8 Demerit Points"
+} else if (speed >= 105){
+  document.getElementById("answer").innerHTML = "7 Demerit Points"
+} else if (speed >= 100){
+  document.getElementById("answer").innerHTML = "6 Demerit Points"
+} else if (speed >= 95){
+  document.getElementById("answer").innerHTML = "5 Demerit Points"
+} else if (speed >= 90){
+  document.getElementById("answer").innerHTML = "4 Demerit Points"
+} else if (speed >= 85){
+  document.getElementById("answer").innerHTML = "3 Demerit Points"
+} else if (speed >= 80){
+  document.getElementById("answer").innerHTML = "2 Demerit Points"
+} else if (speed >= 75){
+  document.getElementById("answer").innerHTML = "1 Demerit Point"
+} else (speed >= 0){
+  document.getElementById("answer").innerHTML = "No Demerit Points"
 }
 
-function main () {
-  let firstNumber, secondNumber;
-  firstNumber = document.getElementByID("textfield1").value;
-  secondNumber = document.getElementByID("textfield2").value;
 
 
-  document.getElementByID("validityTest1").innerHTML = alert (testNAN (firstNumber) );
-  document.getElementByID("validityTest2").innerHTML = testNAN (firstNumber);
-  document.getElementByID("validityTest1").innerHTML = alert (testNAN (secondNumber) );
-  document.getElementByID("validityTest2").innerHTML = testNAN (secondNumber);
-}
 
-function testNAN (number) {
-  if ( isNAN (number) ) { //
-    stop = ture;
-    return "Type a Real Number"
-  } else {
-    return "Input Validated"
-  }
 
 }
