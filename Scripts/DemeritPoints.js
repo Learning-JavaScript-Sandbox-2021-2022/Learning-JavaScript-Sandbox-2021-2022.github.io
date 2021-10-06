7e6c0cdddf997b6ff4845dbd98436a6adac14c62
AhmetAtila <ahmetmatila@gmail.com>
2021-10-05 20:57:26 -0700

    update

--- Scripts/DemeritPoints.js
+++ Scripts/DemeritPoints.js
@@ -2,34 +2,38 @@ document.getElementById("inputButton").addEventListener("click", speedFunction)
 
 function speedFunction() {
 
-if (speed >= 130){
+let answerInuput = document.getElementById("speed").value;
+
+if (answerInuput >= 130){
   document.getElementById("answer").innerHTML = "Your Lisence is Suspended";
-} else if (speed >= 125){
+} else if (answerInuput >= 125){
   document.getElementById("answer").innerHTML = "11 Demerit Points";
-} else if (speed >= 120){
+} else if (answerInuput >= 120){
   document.getElementById("answer").innerHTML = "10 Demerit Points"
-} else if (speed >= 115){
+} else if (answerInuput >= 115){
   document.getElementById("answer").innerHTML = "9 Demerit Points"
-} else if (speed >= 110){
+} else if (answerInuput >= 110){
   document.getElementById("answer").innerHTML = "8 Demerit Points"
-} else if (speed >= 105){
+} else if (answerInuput >= 105){
   document.getElementById("answer").innerHTML = "7 Demerit Points"
-} else if (speed >= 100){
+} else if (answerInuput >= 100){
   document.getElementById("answer").innerHTML = "6 Demerit Points"
-} else if (speed >= 95){
+} else if (answerInuput >= 95){
   document.getElementById("answer").innerHTML = "5 Demerit Points"
-} else if (speed >= 90){
+} else if (answerInuput >= 90){
   document.getElementById("answer").innerHTML = "4 Demerit Points"
-} else if (speed >= 85){
+} else if (answerInuput >= 85){
   document.getElementById("answer").innerHTML = "3 Demerit Points"
-} else if (speed >= 80){
+} else if (answerInuput >= 80){
   document.getElementById("answer").innerHTML = "2 Demerit Points"
-} else if (speed >= 75){
+} else if (answerInuput >= 75){
   document.getElementById("answer").innerHTML = "1 Demerit Point"
-} 
-
-
-
-
+} else if (answerInuput >= 71){
+  document.getElementById("answer").innerHTML = "No Demerit Points yet"
+} else if (answerInuput == 70){
+  document.getElementById("answer").innerHTML = "Your going the speed limit!"
+} else {
+  document.getElementById("answer").innerHTML = "Following the law!"
+}
 
 }
